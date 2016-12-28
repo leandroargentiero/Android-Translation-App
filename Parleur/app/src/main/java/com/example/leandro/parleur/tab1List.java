6e8c1289e2175758c6a6a6486d10cd2418c12f88
leandroargentiero <argentieroleo@gmail.com>
2016-12-28 14:51:10 +0100

    Custom row layout for list view

--- Parleur/app/src/main/java/com/example/leandro/parleur/tab1List.java
+++ Parleur/app/src/main/java/com/example/leandro/parleur/tab1List.java
@@ -8,21 +8,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.leandro.parleur.Models.Word;
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class tab1List extends Fragment{
 
@@ -35,7 +29,7 @@ public class tab1List extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.tab1_list, container, false);
+        View view = inflater.inflate(R.layout.tab1_list, container, false);
 
         // Get ListView by ID
         mListView = (ListView)view.findViewById(R.id.listView_list);
@@ -46,13 +40,13 @@ public class tab1List extends Fragment{
         //Firebase Adapter
         mAdapter = new FirebaseListAdapter<Word>(
                 getActivity(),
-                Word.class, android.R.layout.simple_list_item_2,
+                Word.class, R.layout.custom_row_listview,
                 mDatabase
         ) {
             @Override
             protected void populateView(View v, Word model, int position) {
-                ((TextView)v.findViewById(android.R.id.text1)).setText(model.getWoord());
-                ((TextView)v.findViewById(android.R.id.text2)).setText(model.getVertaling());
+                ((TextView)v.findViewById(R.id.txtWoord)).setText(model.getWoord());
+                ((TextView)v.findViewById(R.id.txtVertaling)).setText(model.getVertaling());
             }
         };
         mListView.setAdapter(mAdapter);
